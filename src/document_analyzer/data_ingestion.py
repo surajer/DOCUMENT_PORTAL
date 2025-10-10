0688a82df1998c3d2f063ac0d18be103316e8c2d
Sujata Yadav <sujatayadav2600@gmail.com>
2025-10-10 19:35:41 +0530

    document analyser and ingestion created

---
+++ src/document_analyzer/data_ingestion.py
@@ -0,0 +1,20 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentHandler:
+    """
+    Handle pdf saving and reading operations.
+    Automatically logs all the actions and supports session-based organization
+    """
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
