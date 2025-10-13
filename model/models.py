3833fd8a7113249fa2c6bb8c34d4937744625eb1
Sujata Yadav <sujatayadav2600@gmail.com>
2025-10-13 13:22:52 +0530

    updated with test.py, models.py and prompt library

--- model/models.py
+++ model/models.py
@@ -1,2 +1,13 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List, dict, Any 
+
+class Metadata(BaseModel):
+    Summary: List[str]
+    Title: str
+    Author: str
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]
+    SentimentTone: str
